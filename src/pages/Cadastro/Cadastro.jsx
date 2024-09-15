6370662085bbeb2f1d3ede747f2d8e9e75368d89
me-lucas-al <lucasalsouza2006@gmail.com>
2024-09-15 17:05:06 -0300

    commit cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -8,7 +8,7 @@ function Cadastro() {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   function onSubmit(data) {
-    navigate("/Confirmacao");
+    navigate("/Confirmacao", { state: { email: data.email } });
   }
 
   return (
@@ -16,59 +16,48 @@ function Cadastro() {
       <main className="flex-grow flex items-center justify-center bg-[#E5E5E5] p-4 w-96">
         <form
           className="bg-white rounded-lg shadow-xl p-8 w-full max-w-md sm:max-w-lg lg:max-w-lg"
-          onSubmit={handleSubmit(onSubmit)} 
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="nome"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nome">
               Nome
             </label>
             <input
               type="text"
               id="nome"
-              className={`${errors?.name ? "border-red-500" : "border-gray-300"} border border-gray-300 rounded w-full py-2 px-3 text-gray-700`}
+              className={`${errors?.name ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
               {...register("name", { required: true })}
             />
-            {errors?.name && <p className="text-red-500 text-xs">Preencha seu nome</p>}
+            {errors?.name?.type === 'required' && <p className="text-red-500 text-xs">Preencha seu nome</p>}
           </div>
 
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className={`${errors?.email ? "border-red-500" : "border-gray-300"} border border-gray-300 rounded w-full py-2 px-3 text-gray-700`}
+              className={`${errors?.email ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
               {...register("email", { required: true })}
             />
-            {errors?.email && <p className="text-red-500 text-xs">Preencha seu e-mail</p>}
+            {errors?.email?.type === 'required' && <p className="text-red-500 text-xs">Preencha seu e-mail</p>}
           </div>
 
           <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="tipoUsuario"
-            >
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tipoUsuario">
               Tipo de Usuário
             </label>
             <select
               id="tipoUsuario"
-              className={`${errors?.user ? "border-red-500" : "border-gray-300"} border border-gray-300 rounded w-full py-2 px-3 text-gray-700`}
-              {...register("user", {
-                validate: (value) => value !== "0" || "Preencha o seu tipo de usuário"
-              })}
+              className={`${errors?.user ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
+              {...register("user", { validate: (value) => value !== "0" })}
             >
-              <option value="0" selected disabled>Selecione seu tipo de usuário...</option>
+              <option value="0">Selecione seu tipo de usuário...</option>
               <option value="oficina">Oficina</option>
               <option value="fornecedor">Fornecedor</option>
             </select>
-            {errors?.user && <p className="text-red-500 text-xs">{errors.user.message}</p>}
+            {errors?.user?.type === 'validate' && <p className="text-red-500 text-xs">Preencha o seu tipo de usuário</p>}
           </div>
 
           <button
--- src/pages/Cadastro/Confirmacao.jsx
+++ src/pages/Cadastro/Confirmacao.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+
 function Confirmacao() {
     const navigate = useNavigate();
 
@@ -7,13 +8,19 @@ function Confirmacao() {
         navigate("/");
     };
 
+    const location = useLocation();
+    const email = location.state?.email || "";
+  
+    const maskedEmail = email.replace(/(?<=.{4}).(?=.*@)/g, "*");
+  
+
     return (
         <div className="flex flex-col">
             <main className="flex-grow flex items-center justify-center bg-[#E5E5E5] p-4">
                 <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md sm:max-w-lg lg:max-w-xl text-center">
                     <h2 className="text-2xl font-bold mb-6">E-mail enviado!</h2>
                     <p className="text-gray-700 mb-6">
-                        Um e-mail foi enviado para *********leme@gmail.com, por favor confirme seu acesso e confirme seu usuário.
+                        Um e-mail foi enviado para {maskedEmail}, por favor confirme seu acesso e confirme seu usuário.
                     </p>
                     <button
                         onClick={handleCadastro}
