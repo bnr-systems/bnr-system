16c13dcdc7a00bba219d83acf1e4aabb6cf8700a
me-lucas-al <lucasalsouza2006@gmail.com>
2024-09-16 20:37:28 -0300

    commit cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import validator from "validator";
+import eyeOn from "/src/assets/images/eye.svg";
+import eyeOff from "/src/assets/images/eye-off.svg";
 
 function Cadastro() {
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors }, watch } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const password = watch("password");
 
   function onSubmit(data) {
     navigate("/Confirmacao", { state: { email: data.email } });
   }
 
-  function handleKeyDown(e){
+  function handleKeyDown(e) {
     if (e.key === 'Enter') {
       handleSubmit(onSubmit)(); 
     }
-  };
+  }
+
+  const toggleShowPassword = () => setShowPassword(!showPassword);
+  const toggleShowPasswordConfirmation = () => setShowPasswordConfirmation(!showPasswordConfirmation);
+
   return (
     <div className="flex flex-col p-0 m-0">
       <main className="flex-grow flex items-center justify-center bg-[#E5E5E5] p-4 w-96">
@@ -23,6 +33,7 @@ function Cadastro() {
           className="bg-white rounded-lg shadow-xl p-8 w-full max-w-md sm:max-w-lg lg:max-w-lg"
           onSubmit={handleSubmit(onSubmit)}
           onKeyDown={handleKeyDown}
+          noValidate
         >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nome">
@@ -39,15 +50,66 @@ function Cadastro() {
 
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
+              E-mail
             </label>
             <input
               type="email"
               id="email"
               className={`${errors?.email ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
-              {...register("email", { required: true })}
+              {...register("email", { 
+                required: true, 
+                validate: (value => validator.isEmail(value)),
+              })}
             />
             {errors?.email?.type === 'required' && <p className="text-red-500 text-xs">Preencha seu e-mail</p>}
+            {errors?.email?.type === 'validate' && <p className="text-red-500 text-xs">E-mail inválido</p>}
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Senha
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                className={`${errors?.password ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
+                {...register("password", { required: true, minLength: 7 })}
+              />
+              <img
+                src={showPassword ? eyeOff : eyeOn}
+                alt="eye-icon"
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                onClick={toggleShowPassword}
+              />
+            </div>
+            {errors?.password?.type === 'required' && <p className="text-red-500 text-xs">Preencha sua senha</p>}
+            {errors?.password?.type === 'minLength' && <p className="text-red-500 text-xs">A senha precisa ter no mínimo 7 caracteres</p>}
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="passwordConfirmation">
+              Confirme sua senha
+            </label>
+            <div className="relative">
+              <input
+                type={showPasswordConfirmation ? "text" : "password"}
+                id="passwordConfirmation"
+                className={`${errors?.passwordConfirmation ? "border-red-500" : "border-gray-400"} border rounded w-full py-2 px-3 text-gray-700`}
+                {...register("passwordConfirmation", { 
+                  required: true, 
+                  validate: value => value === password || "As senhas não coincidem"
+                })}
+              />
+              <img
+                src={showPasswordConfirmation ? eyeOff : eyeOn}
+                alt="eye-icon"
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                onClick={toggleShowPasswordConfirmation}
+              />
+            </div>
+            {errors?.passwordConfirmation?.type === 'required' && <p className="text-red-500 text-xs">Confirme sua senha</p>}
+            {errors?.passwordConfirmation?.type === 'validate' && <p className="text-red-500 text-xs">As senhas não coincidem</p>}
           </div>
 
           <div className="mb-4">
