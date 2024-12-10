7b11e46eb4d5856bdce82822680b3a00ba93acf9
me-lucas-al <me.lucasalmeida.com>
2024-12-10 15:15:45 -0300

    api

--- bnr-system/src/main.jsx
+++ bnr-system/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
-import Cadastro  from './components/cadastro/cadastro'
+import Cadastro  from './pages/Cadastro/cadastro'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
